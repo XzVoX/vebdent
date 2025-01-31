3cd67e9139bbdc6708328e384fa2e0093d98ba71
XzVoX <vujic.zvonimir@gmail.com>
2025-01-31 16:59:12 +0100

    Promjena u padajucem izborniku

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function toggleMenu() {
     const menu = document.querySelector('.nav-list');
     menu.classList.toggle('show');
 }
+
+// Osigurava da se navigacija pravilno prikazuje prilikom promjene veličine prozora
+window.addEventListener('resize', () => {
+    const menu = document.querySelector('.nav-list');
+    const menuIcon = document.querySelector('.menu-icon');
+    if (window.innerWidth > 768) {
+        menu.classList.remove('show');
+    }
+});
