74a6624d05a97d5fc56064754866020bc1d28e19
XzVoX <vujic.zvonimir@gmail.com>
2025-01-31 16:42:34 +0100

    Opis izmena

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll(".nav-link");
-
-    links.forEach(link => {
-        link.addEventListener("click", function () {
-            links.forEach(l => l.classList.remove("active"));
-            this.classList.add("active");
-        });
-    });
-});
 function toggleMenu() {
     const menu = document.querySelector('.nav-list');
     menu.classList.toggle('show');
